d23aea5e870cf8b47bef748d17e03421787756a3
Ben Caudle <ben@happyinspector.com>
2016-04-04 12:33:03 -0700

    Rebuilt project so that it builds an iOS Framework
    * Also fixed a deprecated function warning for -[NSData getBytes:]

---
+++ ZipKit.h
@@ -0,0 +1,24 @@
+//
+//  ZipKit.h
+//  ZipKit
+//
+//  Created by Ben Caudle on 31/03/2016.
+//  Copyright © 2016 Karl Moskowski. All rights reserved.
+//
+
+@import Foundation;
+
+
+//! Project version number for ZipKit.
+FOUNDATION_EXPORT double ZipKitVersionNumber;
+
+//! Project version string for ZipKit.
+FOUNDATION_EXPORT const unsigned char ZipKitVersionString[];
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZipKit/PublicHeader.h>
+#import <ZipKit/ZKDefs.h>
+
+#import <ZipKit/ZKArchive.h>
+#import <ZipKit/ZKDataArchive.h>
+#import <ZipKit/ZKFileArchive.h>
--- ZipKit/ZKDefs.h
+++ ZipKit/ZKDefs.h
@@ -5,6 +5,9 @@
 //  Created by Karl Moskowski on 01/04/09.
 //
 
+@import Foundation;
+
+
 #define ZK_TARGET_OS_MAC (TARGET_OS_MAC && !(TARGET_OS_EMBEDDED || TARGET_OS_IPHONE))
 #define ZK_TARGET_OS_IPHONE (TARGET_OS_EMBEDDED || TARGET_OS_IPHONE || TARGET_OS_IPHONE_SIMULATOR)
 
